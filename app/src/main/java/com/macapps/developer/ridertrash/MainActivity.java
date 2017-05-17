37115892681d9aed56ec1cb84564bb64c6ba1b0d
Developer <andres3741@gmail.com>
2017-05-17 00:32:29 -0500

    Descarga paradas y se muestran que rutas pasan por ahi

--- app/src/main/java/com/macapps/developer/ridertrash/MainActivity.java
+++ app/src/main/java/com/macapps/developer/ridertrash/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -17,8 +19,10 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -27,20 +31,29 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import static android.os.Build.VERSION_CODES.M;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener,OnMarkerClickListener {
     private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference reference;
+    private DatabaseReference reference, paradasRef;
     private Location mLastLocation;
     private LocationRequest mLocationRequest;
     private GoogleApiClient mGoogleApiClient;
     private GoogleMap mMap;
     private Marker mCurrLocationMarker;
+    private String paradaStr;
+    private ArrayList<LatLng> paradasLatLngs;
+    private boolean ready;//Verifica que ya este listo la cadena de parada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +61,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
         firebaseDatabase = FirebaseDatabase.getInstance();
         reference = firebaseDatabase.getReference("driver");
+        paradasRef =firebaseDatabase.getReference("paradas");
+
 
         if (android.os.Build.VERSION.SDK_INT >= M) {
             checkLocationPermission();
         }
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        paradasLatLngs=new ArrayList<>();
         mapFragment.getMapAsync(this);
         ChildEventListener childEventListener = new ChildEventListener() {
             @Override
@@ -101,10 +117,68 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         };
         reference.addChildEventListener(childEventListener);
+
+        paradasRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                paradaStr=dataSnapshot.getValue(Object.class).toString();
+                ready=true;
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });//Descarga la info de una parada
     }
 
     /*
-    OnMapReadyCallback
+    Metodos para decodificar la info de una parada
+
+     */
+    public void decodeParada(View view){
+        if(ready){
+            //Toast.makeText(this, "Bajando Paradas  ", Toast.LENGTH_SHORT).show();
+
+            JSONObject jsonObject,jsonObject1;
+            JSONArray jsonArray1;
+            String paradasStr="parada";
+            List<HashMap> hashMaps= new ArrayList<>();
+            try{
+                jsonObject1=new JSONObject(paradaStr);
+                Toast.makeText(this, "lngth: "+jsonObject1.length(), Toast.LENGTH_SHORT).show();
+
+              //
+                for (Integer j=0;j<=jsonObject1.length()-1;j++) {
+
+                    paradasStr=paradasStr+j.toString();
+                    Toast.makeText(this, paradasStr, Toast.LENGTH_SHORT).show();
+                    jsonObject=jsonObject1.getJSONObject(paradasStr);
+                    String Lat = jsonObject.getString("lat");
+                    String Lng = jsonObject.getString("lng");
+                    LatLng latLng = new LatLng(Double.parseDouble(Lat), Double.parseDouble(Lng));
+                    paradasLatLngs.add(latLng);
+                    Marker marker=mMap.addMarker(new MarkerOptions().position(latLng).title(paradasStr).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+                    jsonArray1 = jsonObject.getJSONArray("rutas");
+                    for (int i = 0; i <= jsonArray1.length() - 1; i++) {
+                    //    Toast.makeText(this, jsonArray1.get(i).toString(), Toast.LENGTH_SHORT).show();
+                    }
+                    paradasStr="parada";
+                }
+
+
+            }catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /*
+    OnMapReadyCallback metodos de GoogleMaps
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -117,6 +191,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     == PackageManager.PERMISSION_GRANTED) {
                 buildGoogleApiClient();
                 mMap.setMyLocationEnabled(true);
+                mMap.setOnMarkerClickListener(this);
             }
         } else {
             buildGoogleApiClient();
@@ -205,4 +280,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Toast.makeText(this, "Name: "+marker.getTitle(), Toast.LENGTH_SHORT).show();
+        return true;
+    }
 }
